4e9e33fca2d2861dd3cbc2ed4ecb4c0c528997ad
Jorge Arbelaez Carmona <jorge.a.arbelaez.c@gmail.com>
2023-02-20 22:23:47 -0500

    finish product model

--- models/ProductModel.js
+++ models/ProductModel.js
@@ -1,10 +1,49 @@
 const mongoose = require("mongoose");
-const productSchema = mongoose.Schema({
-  name: {
-    type: String,
+const productSchema = mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    category: {
+      type: String,
+      required: true,
+    },
+    count: {
+      type: Number,
+      required: true,
+    },
+    price: {
+      type: Number,
+      required: true,
+    },
+    rating: {
+      type: Number,
+    },
+    reviewsNumber: {
+      type: Number,
+    },
+    sales: {
+      type: Number,
+      default: 0,
+    },
+    attrs: [
+      { key: { type: String }, value: { type: String } },
+      // [{ key: "color", value: "red" }, { key: "size", value: "1 TB" }]
+    ],
+    images: [],
+    reviews: [],
   },
-});
-
+  {
+    timestamps: true,
+  }
+);
+productSchema.index();
 const Product = mongoose.model("Product", productSchema);
 
 module.exports = Product;
