fe6c3358f5004c814e0beb7e5011e9411f1f8329
Jorge Arbelaez Carmona <jorge.a.arbelaez.c@gmail.com>
2023-02-22 23:34:14 -0500

    seeder for products

--- seeder/seeder.js
+++ seeder/seeder.js
@@ -2,13 +2,22 @@ const connectDB = require("../config/db");
 connectDB();
 
 const categoryData = require("./categories");
+const productData = require("./products");
+
 const Category = require("../models/CategoryModel");
+const Product = require("../models/ProductModel");
 
 const importData = async () => {
   try {
     await Category.collection.dropIndexes();
+    await Product.collection.dropIndexes();
+
     await Category.collection.deleteMany({});
+    await Product.collection.deleteMany({});
+
     await Category.insertMany(categoryData);
+    await Product.insertMany(productData);
+
     console.log("Seeder data proceeded successfully");
     process.exit();
   } catch (error) {
