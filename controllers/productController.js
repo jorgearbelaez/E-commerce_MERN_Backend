dd9a5ee199d66964457fa5ae3d63a363cc4a8068
Jorge Arbelaez Carmona <jorge.a.arbelaez.c@gmail.com>
2023-02-26 01:25:30 -0500

    Get products endpoint

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,7 +1,14 @@
 const Product = require("../models/ProductModel");
+const recordsPerPage = require("../config/pagination");
 
-const getProducts = (req, res) => {
-  Product.create({ name: "Panasonic" });
-  res.send("Handling product routes, e.g. search for products");
+const getProducts = async (req, res, next) => {
+  try {
+    const products = await Product.find({})
+      .sort({ name: 1 })
+      .limit(recordsPerPage);
+    res.json({ products });
+  } catch (error) {
+    next(error);
+  }
 };
 module.exports = getProducts;
