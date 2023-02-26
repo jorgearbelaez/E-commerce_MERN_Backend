ab62bd6ef3eb7be7735088b0bc303a068a074f3b
Jorge Arbelaez Carmona <jorge.a.arbelaez.c@gmail.com>
2023-02-25 22:16:31 -0500

    Create delete category endpoint

--- routes/categoryRoutes.js
+++ routes/categoryRoutes.js
@@ -3,9 +3,11 @@ const router = express.Router();
 const {
   getCategories,
   newCategory,
+  deleteCategory,
 } = require("../controllers/categoryController");
 
 router.get("/", getCategories);
 router.post("/", newCategory);
+router.delete("/:category", deleteCategory);
 
 module.exports = router;
