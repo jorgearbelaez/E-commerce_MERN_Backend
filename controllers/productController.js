fb4237b6fbd408767d3a4481f22f1f3ebaa6a53f
Jorge Arbelaez Carmona <jorge.a.arbelaez.c@gmail.com>
2023-02-26 15:33:45 -0500

    create price and ratings filters

--- controllers/productController.js
+++ controllers/productController.js
@@ -3,8 +3,29 @@ const recordsPerPage = require("../config/pagination");
 
 const getProducts = async (req, res, next) => {
   try {
+    let query = {};
+    let queryCondition = false;
+
+    let priceQueryCondition = {};
+
+    if (req.query.price) {
+      queryCondition = true;
+      priceQueryCondition = { price: { $lte: Number(req.query.price) } };
+    }
+
+    let ratingQueryCondition = {};
+    if (req.query.rating) {
+      queryCondition = true;
+      ratingQueryCondition = { rating: { $in: req.query.rating.split(",") } };
+    }
+    if (queryCondition) {
+      query = {
+        $and: [priceQueryCondition, ratingQueryCondition],
+      };
+    }
+
+    //pagination
     const pageNum = Number(req.query.pageNum) || 1;
-    const totalProducts = await Product.countDocuments({});
 
     // sort by name, price etc.
     let sort = {};
@@ -15,7 +36,8 @@ const getProducts = async (req, res, next) => {
       sort = { [sortOpt[0]]: Number(sortOpt[1]) };
     }
 
-    const products = await Product.find({})
+    const totalProducts = await Product.countDocuments({});
+    const products = await Product.find(query)
       .skip(recordsPerPage * (pageNum - 1))
       .sort(sort)
       .limit(recordsPerPage);
