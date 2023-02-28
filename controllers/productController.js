cfd22524240da896edf1b58f3bb6c2096f7fb5d8
Jorge Arbelaez Carmona <jorge.a.arbelaez.c@gmail.com>
2023-02-28 18:29:43 -0500

    finish upload file endpoint

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,4 +1,6 @@
 const Product = require("../models/ProductModel");
+const path = require("path");
+const { v4: uuidv4 } = require("uuid");
 const recordsPerPage = require("../config/pagination");
 const imageValidate = require("../utils/imageValidate");
 
@@ -217,15 +219,41 @@ const adminUpload = async (req, res, next) => {
     if (!req.files || !!req.files.images === false) {
       return res.status(400).send("No files were uploaded.");
     }
+
     const validateResult = imageValidate(req.files.images);
     if (validateResult.error) {
       return res.status(400).send(validateResult.error);
     }
+
+    const uploadDirectory = path.resolve(
+      __dirname,
+      "../../frontend",
+      "public",
+      "images",
+      "products"
+    );
+
+    let product = await Product.findById(req.query.productId).orFail();
+
+    let imagesTable = [];
     if (Array.isArray(req.files.images)) {
-      res.send("You sent " + req.files.images.length + " images");
+      imagesTable = req.files.images;
     } else {
-      res.send("You sent only one image");
+      imagesTable.push(req.files.images);
     }
+
+    for (let image of imagesTable) {
+      var fileName = uuidv4() + path.extname(image.name);
+      var uploadPath = uploadDirectory + "/" + fileName;
+      product.images.push({ path: "/images/products/" + fileName });
+      image.mv(uploadPath, function (err) {
+        if (err) {
+          return res.status(500).send(err);
+        }
+      });
+    }
+    await product.save();
+    return res.send("Files uploaded!");
   } catch (err) {
     next(err);
   }
